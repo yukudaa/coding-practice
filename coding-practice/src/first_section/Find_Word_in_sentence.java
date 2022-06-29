69d36a734b736c52dd1d9a971446873843217902
yukuda <leeyookoo@naver.com>
2022-06-29 12:24:17 +0900

    문장 속 단어

---
+++ coding-practice/src/first_section/Find_Word_in_sentence.java
@@ -0,0 +1,28 @@
+package first_section;
+
+import java.util.Scanner;
+
+public class Find_Word_in_sentence {
+
+    public static String solution(String str) {
+
+        String answer = "";
+        int max = 0;
+//        String s[] = str.split(" ");
+//        for (String t : s) {
+//            int len = t.length();
+//            if (len > max) {
+//                max = len;
+//                answer = t;
+//            }
+//        }
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        System.out.println(solution(str));
+    }
+}
