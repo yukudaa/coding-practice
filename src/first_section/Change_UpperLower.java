b180cc0b8a440516bed4e79144d1fd3478ead07d
yukuda <leeyookoo@naver.com>
2022-07-02 23:20:48 +0900

    대소문자 변환

---
+++ src/first_section/Change_UpperLower.java
@@ -0,0 +1,30 @@
+package first_section;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Change_UpperLower {
+
+    public static String solution(String str){
+
+        String answer = "";
+
+        for(char a : str.toCharArray()){
+            if(Character.isUpperCase(a)){
+                answer += Character.toLowerCase(a);
+            }
+            else {
+                answer += Character.toUpperCase(a);
+            }
+        }
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(Change_UpperLower.solution(str));
+    }
+}
