aa455653cc2a6853b39baabeb26042735872ce6d
yukuda <leeyookoo@naver.com>
2022-07-07 23:48:31 +0900

    가장 짧은 문자거리

---
+++ src/first_section/Word_Length.java
@@ -0,0 +1,42 @@
+package first_section;
+
+import java.util.Scanner;
+
+public class Word_Length {
+
+    public static int[] solution(String str, char t) {
+        int[] answer = new int[str.length()];
+        int p = 1000;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == t) {
+                p = 0;
+                answer[i] = p;
+            } else {
+                p++;
+                answer[i] = p;
+            }
+        }
+        p = 1000;
+        for (int i = str.length() - 1; i >= 0; i--) {
+            if (str.charAt(i) == t) {
+                p = 0;
+            } else {
+                p++;
+                answer[i] = Math.min(answer[i], p);
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char c = sc.next().charAt(0);
+        for (int x : Word_Length.solution(str, c)) {
+            System.out.print(x + " ");
+        }
+
+    }
+
+}
