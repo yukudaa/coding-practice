a53a23797a0f1ccd7d2c6a8dd5a3676f1586410b
yukuda <leeyookoo@naver.com>
2022-07-11 22:46:25 +0900

    회문 찾기

---
+++ src/first_section/If_Palindrome.java
@@ -0,0 +1,37 @@
+package first_section;
+
+import java.util.Scanner;
+
+public class If_Palindrome {
+
+    public static String solution(String str) {
+//        String answer = "YES";
+//        str = str.toUpperCase();
+//        int len = str.length();
+//        for (int i = 0; i < len / 2; i++) {
+//            if (str.charAt(i) != str.charAt(len - i - 1)) {
+//                return "NO";
+//            }
+//        }
+//        return answer;
+//    }
+
+        String answer = "NO";
+        String tmp = new StringBuilder(str).reverse().toString();
+        if (str.equalsIgnoreCase(tmp)) {  // 대소문자 무시
+            answer = "YES";
+        }
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(If_Palindrome.solution(str));
+
+    }
+
+
+}
