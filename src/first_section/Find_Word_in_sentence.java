bdb8b44dd80ea45781d984169a0c3794377ab217
yukuda <leeyookoo@naver.com>
2022-07-11 22:46:13 +0900

    문장 속 단어 찾기

---
+++ src/first_section/Find_Word_in_sentence.java
@@ -0,0 +1,32 @@
+package first_section;
+
+import java.util.Scanner;
+
+public class Find_Word_in_sentence {
+
+    public static String solution(String str) {
+        String answer = "";
+
+        int max = 0;
+        String st[] = str.split(" ");
+        for(String a : st) {
+            int len = a.length();
+            if(len > max) {
+                max = len;
+                answer = a;
+            }
+
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        System.out.println(Find_Word_in_sentence.solution(str));
+
+    }
+}
