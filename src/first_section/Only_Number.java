ef748507b80f7b9065201cd722f2e1258b1491ed
yukuda <leeyookoo@naver.com>
2022-07-11 22:46:38 +0900

    숫자만 찾기

---
+++ src/first_section/Only_Number.java
@@ -0,0 +1,25 @@
+package first_section;
+
+import java.util.Scanner;
+
+public class Only_Number {
+
+    public static int solution (String str) {
+        String answer = "";
+        for(char x : str.toCharArray()){
+//            if (x>= 48 && x<= 57){
+//                answer = answer *10 + (x -48);
+//            }
+            if (Character.isDigit(x)) {
+                answer += x;
+            }
+        }
+        return Integer.parseInt(answer);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(Only_Number.solution(str));
+    }
+}
