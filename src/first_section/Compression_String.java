88ee7c411fdc7a89279cdccb5c142878e4594212
yukuda <leeyookoo@naver.com>
2022-07-12 18:39:31 +0900

    문자열 압축

---
+++ src/first_section/Compression_String.java
@@ -0,0 +1,33 @@
+package first_section;
+
+import java.util.Scanner;
+
+public class Compression_String {
+
+    public static String solution(String str) {
+
+        String answer = "";
+        str = str + " ";
+        int cnt = 1;
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.charAt(i) == str.charAt(i + 1)) {
+                cnt++;
+            } else {
+                answer += str.charAt(i);
+                if (cnt > 1) {
+                    answer += cnt;
+                }
+                cnt = 1;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(Compression_String.solution(str));
+    }
+
+}
