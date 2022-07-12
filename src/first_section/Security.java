84989e5153f86c096c2ac5ee62666758c8668f4a
yukuda <leeyookoo@naver.com>
2022-07-12 18:41:34 +0900

    암호

---
+++ src/first_section/Security.java
@@ -0,0 +1,28 @@
+package first_section;
+
+import java.util.Scanner;
+
+public class Security {
+
+    public static String solution(int num,String str) {
+        String answer = "";
+
+        for(int i = 0; i < num; i++){
+            String tmp = str.substring(0,7).replace('#','1').replace('*','0');
+            int n = Integer.parseInt(tmp,2);
+            answer += (char)n;
+            str = str.substring(7);
+        }
+
+
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        String str = sc.next();
+        System.out.println(Security.solution(num,str));
+    }
+}
