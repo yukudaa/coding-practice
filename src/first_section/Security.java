89c89189f329fce14c2bfa0597147a47154c1974
yukuda <leeyookoo@naver.com>
2022-07-16 22:25:55 +0900

    보안

--- src/first_section/Security.java
+++ src/first_section/Security.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 
 public class Security {
 
-    public static String solution(int num,String str) {
+    public static String solution(int num, String str) {
         String answer = "";
 
-        for(int i = 0; i < num; i++){
-            String tmp = str.substring(0,7).replace('#','1').replace('*','0');
-            int n = Integer.parseInt(tmp,2);
-            answer += (char)n;
+        for (int i = 0; i < num; i++) {
+            String tmp = str.substring(0, 7).replace('#', '1').replace('*', '0');
+            int n = Integer.parseInt(tmp, 2);
+            answer += (char) n;
             str = str.substring(7);
         }
 
@@ -23,6 +23,6 @@ public class Security {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
         String str = sc.next();
-        System.out.println(Security.solution(num,str));
+        System.out.println(Security.solution(num, str));
     }
 }
