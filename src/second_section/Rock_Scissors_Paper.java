53116bfc3ffa53b976c862378b7428ba4115959a
yukuda <leeyookoo@naver.com>
2022-07-29 14:16:53 +0900

    가위바위보

--- src/second_section/Rock_Scissors_Paper.java
+++ src/second_section/Rock_Scissors_Paper.java
@@ -20,7 +20,6 @@ public class Rock_Scissors_Paper {
         }
         return answer;
     }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
