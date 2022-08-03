7fd36b2b767daaefa881d20c6f1df4f8e203c2f2
yukuda <leeyookoo@naver.com>
2022-08-03 23:42:38 +0900

    문자열 압축

--- src/first_section/Compression_String.java
+++ src/first_section/Compression_String.java
@@ -23,6 +23,7 @@ public class Compression_String {
         return answer;
     }
 
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
