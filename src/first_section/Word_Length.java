b9b1f033baf069cf7f77d3e551a22c181e325f3d
yukuda <leeyookoo@naver.com>
2022-08-18 15:49:31 +0900

    가장 짧은 문자거리

--- src/first_section/Word_Length.java
+++ src/first_section/Word_Length.java
@@ -30,6 +30,7 @@ public class Word_Length {
         return answer;
     }
 
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
